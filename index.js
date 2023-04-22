cd9baca37be4b368b2818c7f7624924962aac7b3
Nguyễn Ngọc Phước <phuoc.nguyen@heatmob.net>
2023-04-22 18:05:40 +0700

    make it better

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const morgan = require("morgan"); // sử dụng giúp xem được **log** những resquest được gửi lên server.
 const routes = require("./src/router"); //import router
 const connectDB = require("./src/database/connection"); // import file connectDB
+const AdminBro = require('admin-bro')
+const AdminBroExpress = require('@admin-bro/express')
+const AdminBroMongoose = require('@admin-bro/mongoose')
+
 require("dotenv").config(); //thư viện để đọc từ ennv
 
 const bodyParser = require("body-parser"); //Lấy được dữ liệu nhập vào (như trong req.body)
@@ -9,6 +13,10 @@ const path = require("path");
 const app = express();
 const multer = require("multer");
 const clearImage = require("./src/utils/fileUtil");
+const Post = require("./src/models/Post");
+const User = require("./src/models/User");
+const Category = require("./src/models/Category");
+const Recipe = require("./src/models/Recipe");
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, path.join(__dirname, "/public"));
@@ -22,40 +30,52 @@ const storage = multer.diskStorage({
 const uploadMiddleware = multer({
   storage,
 });
-app.use("/public", express.static(path.join(__dirname, "/public")));
-console.log(path.join(__dirname, "/public"));
-//log request on terminal
-app.use(morgan("tiny"));
 
-//mongodb connection
-connectDB();
 
-//parse request to body-parser
-// app.use(express.urlencoded({ limit: '50mb', extended: true }));
-app.use(bodyParser.json({ limit: "50mb" }));
+(async () => {
 
-// import load router
-routes(app);
+  app.use("/public", express.static(path.join(__dirname, "/public")));
+  console.log(path.join(__dirname, "/public"));
+  //log request on terminal
+  app.use(morgan("tiny"));
 
-app.put("/image", uploadMiddleware.single("image"), (req, res, next) => {
-  if (!req.file) {
-    return res.status(200).json({ message: "No file provided!" });
-  }
-  if (req.body.oldPath) {
-    clearImage(req.body.oldPath);
-  }
-  return res.status(201).json({
-    message: "File stored.",
-    payload: {
-      mimetype: req.file.mimetype,
-      filename: req.file.filename,
-      size: req.size,
-    },
+  //mongodb connection
+  await connectDB();
+  //parse request to body-parser
+  // app.use(express.urlencoded({ limit: '50mb', extended: true }));
+  app.use(bodyParser.json({ limit: "50mb" }));
+
+  // import load router
+  routes(app);
+
+  app.put("/image", uploadMiddleware.single("image"), (req, res, next) => {
+    if (!req.file) {
+      return res.status(200).json({ message: "No file provided!" });
+    }
+    if (req.body.oldPath) {
+      clearImage(req.body.oldPath);
+    }
+    return res.status(201).json({
+      message: "File stored.",
+      payload: {
+        mimetype: req.file.mimetype,
+        filename: req.file.filename,
+        size: req.size,
+      },
+    });
   });
-});
 
-const PORT = process.env.PORT || 3000;
+  const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port http://localhost:${PORT}`);
-});
+  AdminBro.registerAdapter({ Resource: AdminBroMongoose.Resource, Database: AdminBroMongoose.Database })
+  const adminBro = new AdminBro({
+    rootPath: '/admin',
+    resources: [Post, User, Category, Recipe],
+  })
+
+  const router = AdminBroExpress.buildRouter(adminBro)
+  app.use(adminBro.options.rootPath, router)
+  app.listen(PORT, () => {
+    console.log(`Example app listening on port http://localhost:${PORT}`);
+  });
+})()
--- src/router/post.js
+++ src/router/post.js
@@ -4,7 +4,9 @@ const validator = require("../validation/posts"); //import validator
 const PostController = require("../app/controllers/PostController"); //import controller
 const { verifyAccessToken } = require("../app/middleware/authentication");
 
-router.post("/upload",verifyAccessToken, validator.post, PostController.store);
+router.get("/", verifyAccessToken, PostController.list);
+router.get("/:id", verifyAccessToken, PostController.detail);
+router.post("/upload", verifyAccessToken, validator.post, PostController.store);
 
 
 module.exports = router;
