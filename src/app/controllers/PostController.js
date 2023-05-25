4a7a9e4976a997b7256ee2acc50a3621df464826
Utiyama <utiyama@cet-app.com>
2023-05-25 11:08:51 +0700

    Fix get all post

--- src/app/controllers/PostController.js
+++ src/app/controllers/PostController.js
@@ -1,5 +1,6 @@
 const Post = require('../../models/Post');
 const { validationResult } = require('express-validator');
+const jwt = require("jsonwebtoken");
 
 const PostController = {
 
@@ -22,9 +23,15 @@ const PostController = {
   async list(req, res) {
     const errors = validationResult(req);
     if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
-    try {
 
-      const data = await Post.find({}).sort({ createdAt: 'descending' })
+    const options = {}
+    if(req.query.contentPost) {
+      options.contentPost = {
+        $regex: '.*' + req.query.contentPost + '.*'
+      }
+    }
+    try {
+      const data = await Post.find(options).sort({ createdAt: 'descending' })
       res.status(200).json({ msg: 'get post list success', data });
 
     } catch (error) {
@@ -33,10 +40,14 @@ const PostController = {
   },
   // [POST] /api/post
   async store(req, res) {
+    const authHeader = req.get("Authorization");
+    const token = authHeader.split(" ")[1];
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
     const errors = validationResult(req);
     if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
     try {
       const {
+        title,
         imagePost,
         contentPost,
         likePost,
@@ -45,7 +56,8 @@ const PostController = {
         statusPost
       } = req.body;
       const newPost = await new Post({
-        idUser: "62c39ca5adbd3436894b82e2",
+        idUser: decodedToken?.id,
+        title: title,
         imagePost: imagePost,
         contentPost: contentPost,
         likePost: likePost,
@@ -59,7 +71,27 @@ const PostController = {
     } catch (error) {
       return res.status(500).json({ errors: [{ msg: error }] });
     }
-  }
+  },
+
+    // [GET] /api/post
+  async getPostCurrentUser(req, res) {
+    const authHeader = req.get("Authorization");
+    const token = authHeader.split(" ")[1];
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    console.log('decodedToken', decodedToken);
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
+
+    try {
+      const data = await Post.find({
+        idUser: req.params.id
+      }).sort({ createdAt: 'descending' });
+      res.status(200).json({ msg: 'get post list success', data });
+
+    } catch (error) {
+      return res.status(500).json({ errors: [{ msg: error }] });
+    }
+  },
 
 }
 
