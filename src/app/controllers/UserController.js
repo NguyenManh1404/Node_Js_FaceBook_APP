e07b45095930c46d732d7d6cc17c51e215e9f6eb
1911505310241 <1911505310241@sv.ute.udn.vn>
2023-05-28 20:58:11 +0700

    add profile

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -4,32 +4,31 @@ const UserController = {
   // [GET] /api/user
   async list(req, res) {
     const errors = validationResult(req);
-    if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
-    
-    const options = {}
-    if(req.query.firstName) {
+    if (!errors.isEmpty())
+      return res.status(400).json({ errors: errors.array() });
+
+    const options = {};
+    if (req.query.firstName) {
       options.firstName = {
-        $regex: '.*' + req.query.firstName + '.*'
-      }
+        $regex: ".*" + req.query.firstName + ".*",
+      };
     }
 
-    if(req.query.lastName) {
+    if (req.query.lastName) {
       options.lastName = {
-        $regex: '.*' + req.query.lastName + '.*'
-      }
+        $regex: ".*" + req.query.lastName + ".*",
+      };
     }
 
-    if(req.query.email) {
+    if (req.query.email) {
       options.email = {
-        $regex: '.*' + req.query.email + '.*'
-      }
+        $regex: ".*" + req.query.email + ".*",
+      };
     }
 
     try {
-
-      const data = await User.find(options).sort({ createdAt: 'descending' })
-      res.status(200).json({ msg: 'get user list success', data });
-
+      const data = await User.find(options).sort({ createdAt: "descending" });
+      res.status(200).json({ msg: "get user list success", data });
     } catch (error) {
       return res.status(500).json({ errors: [{ msg: error }] });
     }
@@ -38,21 +37,21 @@ const UserController = {
   // [GET] /api/popular-creator
   async popularCreator(req, res) {
     const errors = validationResult(req);
-    if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
+    if (!errors.isEmpty())
+      return res.status(400).json({ errors: errors.array() });
     try {
-
       const data = await User.aggregate([
         {
           $lookup: {
-            from: 'post',
-            localField: '_id',
-            foreignField: 'idUser',
-            as: 'post',
+            from: "post",
+            localField: "_id",
+            foreignField: "idUser",
+            as: "post",
           },
         },
         {
           $addFields: {
-            totalLikes: { $sum: '$post.likePost' },
+            totalLikes: { $sum: "$post.likePost" },
           },
         },
         {
@@ -62,11 +61,26 @@ const UserController = {
         },
       ]);
 
-
-      res.status(200).json({ msg: 'get user list success', data });
-
+      res.status(200).json({ msg: "get user list success", data });
     } catch (error) {
       return res.status(500).json({ errors: [{ msg: error }] });
+    }
+  },
+
+  async getProfile(req, res) {
+    const authHeader = req.get("Authorization");
+    const token = authHeader.split(" ")[1];
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+    const data = await User.find({
+      id: decodedToken.id,
+    });
+
+    console.log(data);
+    if (data) {
+      res.status(200).json({ msg: "get user", data });
+    } else {
+      res.status(200).json({ msg: "user not found" });
     }
   },
 };
--- src/router/user.js
+++ src/router/user.js
@@ -1,9 +1,14 @@
-const express = require('express'); //import
+const express = require("express"); //import
 const router = express.Router(); //import
 const UserController = require("../app/controllers/UserController"); //import controller
 const { verifyAccessToken } = require("../app/middleware/authentication");
 
 router.get("/", verifyAccessToken, UserController.list);
-router.get("/popular-creator", verifyAccessToken, UserController.popularCreator);
+router.get(
+  "/popular-creator",
+  verifyAccessToken,
+  UserController.popularCreator
+);
+router.get("/profile", verifyAccessToken, UserController.getProfile);
 
 module.exports = router;
