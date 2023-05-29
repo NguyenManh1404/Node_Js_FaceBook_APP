93ed3384efd0f53cccaf3ea80bac29298d7d5214
Utiyama <utiyama@cet-app.com>
2023-05-29 23:19:46 +0700

    Fix list current user recipe

--- src/router/recipe.js
+++ src/router/recipe.js
@@ -11,6 +11,7 @@ router.get("/diet", verifyAccessToken, RecipeController.getDiet);
 router.get("/recent", verifyAccessToken, RecipeController.getRecentRecipe);
 router.get("/trending", verifyAccessToken, RecipeController.trendingNow);
 router.get("/", verifyAccessToken, RecipeController.list);
+router.get("/get-recipe-current-user", verifyAccessToken, RecipeController.getListRecipeCurrentUser);
 router.put("/:id", verifyAccessToken, RecipeController.edit)
 router.delete("/:id", verifyAccessToken, RecipeController.delete)
 router.get("/detail/:id", verifyAccessToken, RecipeController.getRecipeById);
