21394d13ac480d2a30bb278e95a981249fc620d1
Utiyama <utiyama@cet-app.com>
2023-06-03 23:10:27 +0700

    following

--- src/app/controllers/FollowerController.js
+++ src/app/controllers/FollowerController.js
@@ -134,6 +134,26 @@ const FollowerController = {
         return res.status(500).json({ errors: [{ msg: error }] });
       }
     },
+
+     // [GET] /api/follower/following
+     async listFollowing(req, res) {
+      const authHeader = req.get("Authorization");
+      const token = authHeader.split(" ")[1];
+      const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
+  
+      try {
+        const data = await Follower.find({
+          idUserFollower: decodedToken.id
+        }).sort({ createdAt: 'descending' })
+        res.status(200).json({ msg: 'get follwer list success', data });
+  
+      } catch (error) {
+        return res.status(500).json({ errors: [{ msg: error }] });
+      }
+    },
+
 };
 
 module.exports = FollowerController;
